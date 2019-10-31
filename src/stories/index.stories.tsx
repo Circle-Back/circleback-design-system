17fcacfdeb5bd2b3d9913c8ce2618d4eac667273
Amaro Mariño <1252707+amalv@users.noreply.github.com>
2019-10-31 15:42:44 +0100

    fix: remove welcome story example

--- src/stories/index.stories.tsx
+++ src/stories/index.stories.tsx
@@ -2,10 +2,7 @@ import React from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-import { linkTo } from "@storybook/addon-links";
 import { RoundedButton } from "../components/RoundedButton";
-import { Welcome } from "@storybook/react/demo";
-storiesOf("Welcome", module).add("to Storybook", () => <Welcome showApp={linkTo("RoundedButton")} />);
 
 storiesOf("RoundedButton", module)
   .add(
