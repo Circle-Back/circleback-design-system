8136e89ad72570b2fb305f2e236ec55337cfa33d
Amaro Mariño <1252707+amalv@users.noreply.github.com>
2019-11-01 21:33:38 +0100

    feat: add prettier and eslint
    - Add prettier
    - Add eslint
    - Fix linting errors in src files

--- src/components/RoundedButton.tsx
+++ src/components/RoundedButton.tsx
@@ -8,24 +8,29 @@ export interface RoundedButtonProps {
   children: React.ReactNode;
 }
 
-export function RoundedButton(props: RoundedButtonProps) {
-  return (
-    <button
-      style={{
-        backgroundColor: props.color,
-        border: "none",
-        color: "white",
-        padding: 20,
-        textAlign: "center",
-        textDecoration: "none",
-        display: "inline-block",
-        fontSize: "16px",
-        margin: "4px 2px",
-        borderRadius: 5
-      }}
-      onClick={() => props.onClick()}
-    >
-      {props.children}
-    </button>
-  );
-}
+const RoundedButton: React.FC<RoundedButtonProps> = ({
+  color,
+  children,
+  onClick,
+}: RoundedButtonProps) => (
+  <button
+    type="button"
+    style={{
+      backgroundColor: color,
+      border: "none",
+      color: "white",
+      padding: 20,
+      textAlign: "center",
+      textDecoration: "none",
+      display: "inline-block",
+      fontSize: "16px",
+      margin: "4px 2px",
+      borderRadius: 5,
+    }}
+    onClick={(): void => onClick()}
+  >
+    {children}
+  </button>
+);
+
+export default RoundedButton;
