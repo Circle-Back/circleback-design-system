94970601fa5cad0fa84710793885ff2f6829d289
amalv <1252707+amalv@users.noreply.github.com>
2019-11-08 00:11:30 +0100

    feat: add fullWidth prop for Input component (#35)
    - Add fullWidth prop to Input component
    - Add FullWidthInput story

--- src/components/Input.stories.tsx
+++ src/components/Input.stories.tsx
@@ -7,6 +7,8 @@ export default {
 
 export const DefaultInput: React.FC = () => <Input />;
 
+export const FullWidthInput: React.FC = () => <Input fullWidth />;
+
 export const EmailInput: React.FC = () => (
   <Input label="email" placeholder="my@email.com" borderColor="#6200ee" />
 );
