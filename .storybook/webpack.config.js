42f3b96a4ea358f8adfab82e87133d902896040d
amalv <1252707+amalv@users.noreply.github.com>
2019-11-09 23:50:25 +0100

    feat: add storybook addon source loader
    Show the component code in the addons tab

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,16 +1,29 @@
 module.exports = ({ config }) => {
-  config.module.rules.push({
-    test: /\.tsx?$/,
-    use: [
-      {
-        loader: require.resolve("babel-loader"),
-        options: {
-          presets: [require.resolve("babel-preset-react-app")]
-        }
-      },
-      require.resolve("react-docgen-typescript-loader")
-    ]
-  });
+  config.module.rules.push(
+    {
+      test: /\.tsx?$/,
+      use: [
+        {
+          loader: require.resolve("babel-loader"),
+          options: {
+            presets: [require.resolve("babel-preset-react-app")]
+          }
+        },
+        
+        require.resolve("react-docgen-typescript-loader")
+      ]
+    },
+    {
+      test: /\.stories\.tsx?$/,
+      loaders: [
+        {
+          loader: require.resolve('@storybook/source-loader'),
+          options: { parser: 'typescript' },
+        },
+      ],
+      enforce: 'pre',
+    }
+  );
 
   config.resolve.extensions.push(".ts", ".tsx");
 
