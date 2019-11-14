66bf276913f900d41b98c3b91bcefbfaa66cb938
Amaro Mariño <1252707+amalv@users.noreply.github.com>
2019-11-14 16:46:47 +0100

    fix: add theme provider to filters component
    Bundle the theming with the component to avoid style issues

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -2,17 +2,16 @@ import * as React from "react";
 import Grid from "@material-ui/core/Grid";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
-import { StylesProvider, useTheme } from "@material-ui/core/styles";
+import { StylesProvider, ThemeProvider } from "@material-ui/core/styles";
 import {
   FiltersWrapper,
   FiltersLabel,
   FormControlStyled,
   InputBaseStyled,
 } from "./Filters.styles";
+import theme from "../MuiTheme";
 
 export const Filters: React.FC = () => {
-  const theme = useTheme();
-
   const [category, setCategory] = React.useState("");
   const [user, setUser] = React.useState("");
   const handleChangeCategory = (
@@ -29,53 +28,55 @@ export const Filters: React.FC = () => {
 
   return (
     <StylesProvider injectFirst>
-      <FiltersWrapper color={theme.palette.primary.main}>
-        <Grid item xs container direction="column">
-          <Grid item>
-            <FiltersLabel textcolor={theme.palette.tertiary}>
-              Filter by
-            </FiltersLabel>
+      <ThemeProvider theme={theme}>
+        <FiltersWrapper color={theme.palette.primary.main}>
+          <Grid item xs container direction="column">
+            <Grid item>
+              <FiltersLabel textcolor={theme.palette.tertiary}>
+                Filter by
+              </FiltersLabel>
+            </Grid>
+            <Grid item>
+              <FormControlStyled>
+                <Select
+                  displayEmpty
+                  id="select-category"
+                  value={category}
+                  onChange={handleChangeCategory}
+                  input={
+                    <InputBaseStyled textcolor={theme.palette.text.secondary} />
+                  }
+                >
+                  <MenuItem value="" disabled>
+                    Category
+                  </MenuItem>
+                  <MenuItem value={10}>Category 1</MenuItem>
+                  <MenuItem value={20}>Category 2</MenuItem>
+                  <MenuItem value={30}>Category 3</MenuItem>
+                </Select>
+              </FormControlStyled>
+              <FormControlStyled>
+                <Select
+                  displayEmpty
+                  id="select-user"
+                  value={user}
+                  onChange={handleChangeUser}
+                  input={
+                    <InputBaseStyled textcolor={theme.palette.text.secondary} />
+                  }
+                >
+                  <MenuItem value="" disabled>
+                    User
+                  </MenuItem>
+                  <MenuItem value={10}>User 1</MenuItem>
+                  <MenuItem value={20}>User 2</MenuItem>
+                  <MenuItem value={30}>User 3</MenuItem>
+                </Select>
+              </FormControlStyled>
+            </Grid>
           </Grid>
-          <Grid item>
-            <FormControlStyled>
-              <Select
-                displayEmpty
-                id="select-category"
-                value={category}
-                onChange={handleChangeCategory}
-                input={
-                  <InputBaseStyled textcolor={theme.palette.text.secondary} />
-                }
-              >
-                <MenuItem value="" disabled>
-                  Category
-                </MenuItem>
-                <MenuItem value={10}>Category 1</MenuItem>
-                <MenuItem value={20}>Category 2</MenuItem>
-                <MenuItem value={30}>Category 3</MenuItem>
-              </Select>
-            </FormControlStyled>
-            <FormControlStyled>
-              <Select
-                displayEmpty
-                id="select-user"
-                value={user}
-                onChange={handleChangeUser}
-                input={
-                  <InputBaseStyled textcolor={theme.palette.text.secondary} />
-                }
-              >
-                <MenuItem value="" disabled>
-                  User
-                </MenuItem>
-                <MenuItem value={10}>User 1</MenuItem>
-                <MenuItem value={20}>User 2</MenuItem>
-                <MenuItem value={30}>User 3</MenuItem>
-              </Select>
-            </FormControlStyled>
-          </Grid>
-        </Grid>
-      </FiltersWrapper>
+        </FiltersWrapper>
+      </ThemeProvider>
     </StylesProvider>
   );
 };
