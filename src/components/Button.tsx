7261b5dfcd741eb11d19157e98590af28961a3d2
Amaro Mariño <1252707+amalv@users.noreply.github.com>
2019-11-14 18:53:45 +0100

    fix: add text support for button component
    Use children prop as text to display in button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,11 +3,14 @@ import { ThemeProvider, StylesProvider } from "@material-ui/core/styles";
 import { BaseButton } from "./Button.styles";
 import theme from "../MuiTheme";
 
-export const Button: React.FC = () => (
+export interface ButtonProps {
+  children: React.ReactNode;
+}
+export const Button: React.FC<ButtonProps> = ({ children }: ButtonProps) => (
   <StylesProvider injectFirst>
     <ThemeProvider theme={theme}>
       <BaseButton backgroundcolor={theme.palette.primary.main}>
-        Primary
+        {children}
       </BaseButton>
     </ThemeProvider>
   </StylesProvider>
