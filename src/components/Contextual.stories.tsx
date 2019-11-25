8d79c3b1ee9690d41f4a470f65031bffadb2fc5f
amalv <1252707+amalv@users.noreply.github.com>
2019-11-25 18:45:29 +0100

    feat: add with button story for contextual component (#88)
    The button is displayed overlaying the contextual element

--- src/components/Contextual.stories.tsx
+++ src/components/Contextual.stories.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import Box from "@material-ui/core/Box";
 import { Contextual } from "./Contextual";
 
 export default {
@@ -6,10 +7,24 @@ export default {
 };
 
 export const Default: React.FC = () => (
-  <Contextual
-    name="category"
-    title="Hardware"
-    body="Lorem ipsum dolor sit amet..."
-    image="./hardware.png"
-  />
+  <Box width="344px">
+    <Contextual
+      name="category"
+      title="Hardware"
+      body="Lorem ipsum dolor sit amet..."
+      image="./hardware.png"
+    />
+  </Box>
+);
+
+export const WithButton: React.FC = () => (
+  <Box width="344px">
+    <Contextual
+      name="category"
+      title="Hardware"
+      body="Lorem ipsum dolor sit amet..."
+      image="./hardware.png"
+      button
+    />
+  </Box>
 );
