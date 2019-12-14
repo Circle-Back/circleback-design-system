4d8c111b9187271f81c33b4b7382c7fc44278aa6
Amaro Mariño <1252707+amalv@users.noreply.github.com>
2019-12-14 11:13:05 +0100

    feat: add footer component
    Add footer component with default story

---
+++ src/components/Footer.styles.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import Typography from "@material-ui/core/Typography";
+
+export const BaseFooter = styled.div`
+  padding-top: 39px;
+  height: 290px;
+  border-radius: 4px 4px 0 0;
+  background-color: #6200ee;
+`;
+
+export const FooterTitle = styled(Typography)`
+  color: rgba(255, 255, 255, 0.87);
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 0.15px;
+  line-height: 24px;
+`;
+
+export const FooterElement = styled(Typography)`
+  color: rgba(255, 255, 255, 0.6);
+  font-size: 16px;
+  font-weight: 400;
+  letter-spacing: 0.15px;
+  line-height: 24px;
+`;
+
+export const Divider = styled.div`
+  width: 251px;
+  height: 1px;
+  background-color: #ffffff;
+  opacity: 0.12;
+`;
